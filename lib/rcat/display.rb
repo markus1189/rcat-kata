2e4b6a6fb5c05a364e96133c3922fac7e0f7670c
Markus Hauck <markus1189@gmail.com>
2012-05-26 19:52:33 +0200

    Removed passed args to display, replaced by setters

--- lib/rcat/display.rb
+++ lib/rcat/display.rb
@@ -1,29 +1,41 @@
 module RCat
   class Display
     def initialize(params)
-      @line_numbering_style   = params[:line_numbering_style]
-      @squeeze_extra_newlines = params[:squeeze_extra_newlines]
+      @line_number = 1
+    end
+
+    def enable_numbering
+      @numbering = true
+    end
+
+    def enable_squeeze
+      @squeeze = true
+    end
+
+    def enable_ignore_blank
+      @ignore_blank_lines = true
     end
 
     def render(data)
-      @line_number ||= 1
-
       lines = data.lines
       loop { render_line(lines) }
     end
 
     private
 
-    attr_reader :line_numbering_style, :squeeze_extra_newlines, :line_number
+    attr_reader :numbering, :squeeze, :ignore_blank_lines
 
     def render_line(lines)
-      current_line = lines.next 
+      current_line = lines.next
       current_line_is_blank = current_line.chomp.empty?
+
+      line_numbering_style = :all_lines if numbering && !ignore_blank_lines
+      line_numbering_style = :significant_lines if (numbering && ignore_blank_lines) || ignore_blank_lines
 
       case line_numbering_style
       when :all_lines
         print_labeled_line(current_line)
-        increment_line_number  
+        increment_line_number
       when :significant_lines
         if current_line_is_blank
           print_unlabeled_line(current_line)
@@ -36,13 +48,13 @@ module RCat
         increment_line_number
       end
 
-      if squeeze_extra_newlines && current_line_is_blank
+      if squeeze && current_line_is_blank
          lines.next while lines.peek.chomp.empty?
       end
     end
 
     def print_labeled_line(line)
-      print "#{line_number.to_s.rjust(6)}\t#{line}" 
+      print "#{@line_number.to_s.rjust(6)}\t#{line}"
     end
 
     def print_unlabeled_line(line)
