89ab8ea5ba071b5c06d3ef3b5bf254ebdc6b737e
Markus Hauck <markus1189@gmail.com>
2012-05-26 20:34:13 +0200

    Simplified logic in render_line

--- lib/rcat/display.rb
+++ lib/rcat/display.rb
@@ -2,18 +2,19 @@ module RCat
   class Display
     def initialize(params)
       @line_number = 1
+      @numbering = :none
     end
 
     def enable_numbering
-      @numbering = true
+      @numbering = :all if @numbering != :significant
     end
 
     def enable_squeeze
       @squeeze = true
     end
 
-    def enable_ignore_blank
-      @ignore_blank_lines = true
+    def enable_significant_numbering
+      @numbering = :significant
     end
 
     def render(data)
@@ -23,38 +24,34 @@ module RCat
 
     private
 
-    attr_reader :numbering, :squeeze, :ignore_blank_lines
+    attr_reader :numbering, :squeeze
 
     def render_line(lines)
       current_line = lines.next
       current_line_is_blank = current_line.chomp.empty?
 
-      line_numbering_style = :all_lines if numbering && !ignore_blank_lines
-      line_numbering_style = :significant_lines if (numbering && ignore_blank_lines)
-
-      case line_numbering_style
-      when :all_lines
-        print_labeled_line(current_line)
-        increment_line_number
-      when :significant_lines
+      case numbering
+      when :none
+        print_unlabeled_line(current_line)
+      when :significant
         if current_line_is_blank
           print_unlabeled_line(current_line)
         else
           print_labeled_line(current_line)
-          increment_line_number
         end
-      else
-        print_unlabeled_line(current_line)
-        increment_line_number
+      when :all
+          print_labeled_line(current_line)
+      else fail "Unexpected case: #{numbering}"
       end
 
       if squeeze && current_line_is_blank
-         lines.next while lines.peek.chomp.empty?
+        lines.next while lines.peek.chomp.empty?
       end
     end
 
     def print_labeled_line(line)
       print "#{@line_number.to_s.rjust(6)}\t#{line}"
+      increment_line_number
     end
 
     def print_unlabeled_line(line)
